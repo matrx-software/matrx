a39bbd600c3b002acc99001bc0ea6cbf7b7596a3
Jasper van der Waa <jasper.vanderwaa@tno.nl>
2019-05-14 16:48:33 +0200

    Removed all old mains and old scenario manager

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from scenario_manager.manager import ScenarioManager
+from agents.Agent import Agent
+from environment.objects.simple_objects import Wall, Battery
+from scenario_manager.world_factory import WorldFactory, RandomProperty
 
 if __name__ == "__main__":
+    factory = WorldFactory(random_seed=1, shape=[10, 10], tick_duration=0.5)
 
-    scenario_file = "test_scenario.json"
+    random_prop = RandomProperty(property_name="random_prop", values=["One", "Two"], distribution=[3, 1])
+    factory.add_env_object(location=[0, 0], name="Wall 1", random_prop=random_prop)
 
-    # Create the scenario's grid world
-    grid_world = ScenarioManager().create_scenario(scenario_file=scenario_file)
+    agent = Agent()
+    factory.add_agent(location=[1, 0], agent=agent)
 
-    # Run the grid world
-    grid_world.run()
+    world = factory.get_world()
+
+    world.run()
