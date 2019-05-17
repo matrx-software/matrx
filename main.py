9dd7b2a80d4eb1ae75c1a7574a3485185b7e472c
Jasper van der Waa <jasper.vanderwaa@tno.nl>
2019-05-17 16:22:35 +0200

    Removed old json files, added example use case scenarios, added main.py where use cases are called and run

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-from agents.Agent import Agent
-from environment.objects.simple_objects import Wall, Battery
-from scenario_manager.world_factory import WorldFactory, RandomProperty
+from scenarios import test_scenario, hat_case, blanket_search_case
 
 if __name__ == "__main__":
-    factory = WorldFactory(random_seed=1, shape=[10, 10], tick_duration=0.5)
 
-    random_prop = RandomProperty(property_name="random_prop", values=["One", "Two"], distribution=[3, 1])
-    factory.add_env_object(location=[0, 0], name="Wall 1", random_prop=random_prop)
-
-    agent = Agent()
-    factory.add_agent(location=[1, 0], agent=agent)
-
-    agents = [Agent() for _ in range(3)]
-    locs = [[1, 1], [2, 2], [3, 3]]
-    factory.add_multiple_agents(agents=agents, locations=locs)
-
-    factory.add_multiple_objects([[4, 4], [5, 5], [6, 6]])
+    # By creating scripts that return a factory, we can define infinite number of use cases and select them (in the
+    # future) through a UI.
+    factory = test_scenario.create_factory()
+    # factory = blanket_search_case.create_factory()
+    # factory = hat_case.create_factory()
 
     world = factory.get_world()
 
