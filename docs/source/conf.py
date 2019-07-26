d7faee9ebd65d07346987c95a6b30820f09b9d28
JSW <jasper.vanderwaa@tno.nl>
2019-07-26 09:05:38 +0200

    First doc build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,22 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../../matrxs"))
+print("\n\n -- System Path --> \n")
+for p in sys.path:
+    print(f"{p}")
+print("\n\n")
+
 # -- Project information -----------------------------------------------------
 
 project = 'MATRXS'
