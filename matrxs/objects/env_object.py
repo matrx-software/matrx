aeb67e9fe2dfe400b8db30fb0cf3886d90ba8626
Tjalling Haije <tjalling.haije@tno.nl>
2020-01-16 13:16:22 +0100

    fixed bug with capitalized names not working for GUI links

--- matrxs/objects/env_object.py
+++ matrxs/objects/env_object.py
@@ -76,7 +76,7 @@ class EnvObject:
 
         # Obtain a unique ID based on a global object counter, if not already set as an attribute in a super class
         if not hasattr(self, "obj_id"):
-            self.obj_id = f"{self.obj_name}_{next_obj_id()}"
+            self.obj_id = f"{self.obj_name}_{next_obj_id()}".lower()
 
         # Make customizable_properties mutable if not given.
         if customizable_properties is None:
