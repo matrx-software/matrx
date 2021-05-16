09edb2a24eb19b09c2b1eb2dc4890ecec74877bb
Jasper <jaspervanderwaa@gmail.com>
2021-05-16 13:52:46 +0200

    Added the "location" property as exception to take its value as is

--- matrx/agents/agent_utils/state.py
+++ matrx/agents/agent_utils/state.py
@@ -461,7 +461,8 @@ class State(MutableMapping):
 
         # For each prop_name, prop_value combination, find the relevant objects. If there are more than one allowable
         # property value, search for those as well.
-        found = [[self.__find(name, val) for val in vals] for name, vals in props.items()]
+        found = [[self.__find(name, val) for val in vals] if name != "location" else [self.__find(name, vals)]
+                 for name, vals in props.items()]
 
         # Check if we searched for more than one property
         if len(props) > 1:
